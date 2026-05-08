0ee6b0b9fa9640f3f10a40db7b6b677c7acb4262
Isaac Dobbins <isaac.dobbins@icloud.com>
2026-05-08 15:38:48 -0600

    offscreen images

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #define VK_ENABLE_BETA_EXTENSIONS
 #include <vulkan/vulkan.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include <time.h>
 #include "trace_comp_spv.h"
 #include "platform.h"
@@ -9,6 +10,9 @@
 #define SWAP_IMAGE_COUNT  3
 #define COMPUTE_TILE_SIZE 8
 #define LEN(a)            ((uint32_t)(sizeof(a) / sizeof(*(a))))
+
+static const VkFormat SWAP_FORMAT = VK_FORMAT_B8G8R8A8_UNORM;
+static const VkFormat RENDER_FORMAT = VK_FORMAT_R8G8B8A8_UNORM;
 
 static const char *const INSTANCE_EXTS[] = {
     VK_KHR_SURFACE_EXTENSION_NAME,
@@ -28,7 +32,9 @@ static VkQueue               queue;
 static VkSwapchainKHR        swapchain;
 static VkExtent2D            swapExtent;
 static VkImage               swapImages[SWAP_IMAGE_COUNT];
-static VkImageView           swapImageViews[SWAP_IMAGE_COUNT];
+static VkImage               renderImage;
+static VkDeviceMemory        renderMemory;
+static VkImageView           renderImageView;
 static VkDescriptorSetLayout descriptorSetLayout;
 static VkDescriptorPool      descriptorPool;
 static VkDescriptorSet       descriptorSet;
@@ -47,30 +53,91 @@ static double nowSeconds(void)
     return (double)ts.tv_sec + (double)ts.tv_nsec * 1e-9;
 }
 
-static void recordCommandBuffer(VkCommandBuffer cb, VkDescriptorSet ds, VkImage image, float time)
+static uint32_t memoryType(uint32_t bits, VkMemoryPropertyFlags flags)
 {
-    VkImageSubresourceRange range = {
-        .aspectMask = VK_IMAGE_ASPECT_COLOR_BIT,
-        .levelCount = 1,
-        .layerCount = 1,
+    VkPhysicalDeviceMemoryProperties props;
+    vkGetPhysicalDeviceMemoryProperties(physicalDevice, &props);
+
+    for (uint32_t i = 0; i < props.memoryTypeCount; i++) {
+        if ((bits & (1u << i)) &&
+            (props.memoryTypes[i].propertyFlags & flags) == flags)
+            return i;
+    }
+
+    abort();
+}
+
+static void imageBarrier(
+    VkCommandBuffer cb,
+    VkImage image,
+    VkImageLayout oldLayout,
+    VkImageLayout newLayout,
+    VkAccessFlags srcAccess,
+    VkAccessFlags dstAccess,
+    VkPipelineStageFlags srcStage,
+    VkPipelineStageFlags dstStage)
+{
+    vkCmdPipelineBarrier(cb,
+        srcStage, dstStage,
+        0, 0, NULL, 0, NULL, 1, &(VkImageMemoryBarrier){
+            .sType               = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER,
+            .srcAccessMask       = srcAccess,
+            .dstAccessMask       = dstAccess,
+            .oldLayout           = oldLayout,
+            .newLayout           = newLayout,
+            .srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED,
+            .dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED,
+            .image               = image,
+            .subresourceRange    = {
+                .aspectMask = VK_IMAGE_ASPECT_COLOR_BIT,
+                .levelCount = 1,
+                .layerCount = 1,
+            },
+        });
+}
+
+static void recordCommandBuffer(
+    VkCommandBuffer cb,
+    VkDescriptorSet ds,
+    VkImage renderImage,
+    VkImage swapImage,
+    VkImageLayout renderOldLayout,
+    float time)
+{
+    VkImageBlit blit = {
+        .srcSubresource = {
+            .aspectMask = VK_IMAGE_ASPECT_COLOR_BIT,
+            .layerCount = 1,
+        },
+        .srcOffsets = {
+            { 0, 0, 0 },
+            { (int32_t)swapExtent.width, (int32_t)swapExtent.height, 1 },
+        },
+        .dstSubresource = {
+            .aspectMask = VK_IMAGE_ASPECT_COLOR_BIT,
+            .layerCount = 1,
+        },
+        .dstOffsets = {
+            { 0, 0, 0 },
+            { (int32_t)swapExtent.width, (int32_t)swapExtent.height, 1 },
+        },
     };
 
     vkBeginCommandBuffer(cb, &(VkCommandBufferBeginInfo){
         .sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO,
     });
 
-    vkCmdPipelineBarrier(cb,
-        VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT, VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT,
-        0, 0, NULL, 0, NULL, 1, &(VkImageMemoryBarrier){
-            .sType               = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER,
-            .dstAccessMask       = VK_ACCESS_SHADER_WRITE_BIT,
-            .oldLayout           = VK_IMAGE_LAYOUT_UNDEFINED,
-            .newLayout           = VK_IMAGE_LAYOUT_GENERAL,
-            .srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED,
-            .dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED,
-            .image               = image,
-            .subresourceRange    = range,
-        });
+    imageBarrier(cb,
+        renderImage,
+        renderOldLayout, VK_IMAGE_LAYOUT_GENERAL,
+        0, VK_ACCESS_SHADER_WRITE_BIT,
+        VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT, VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT);
+
+    imageBarrier(cb,
+        swapImage,
+        VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+        0, VK_ACCESS_TRANSFER_WRITE_BIT,
+        VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT, VK_PIPELINE_STAGE_TRANSFER_BIT);
 
     vkCmdBindPipeline(cb, VK_PIPELINE_BIND_POINT_COMPUTE, pipeline);
     vkCmdBindDescriptorSets(cb, VK_PIPELINE_BIND_POINT_COMPUTE, pipelineLayout, 0, 1, &ds, 0, NULL);
@@ -80,20 +147,97 @@ static void recordCommandBuffer(VkCommandBuffer cb, VkDescriptorSet ds, VkImage 
         (swapExtent.height + COMPUTE_TILE_SIZE - 1) / COMPUTE_TILE_SIZE,
         1);
 
-    vkCmdPipelineBarrier(cb,
-        VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT, VK_PIPELINE_STAGE_BOTTOM_OF_PIPE_BIT,
-        0, 0, NULL, 0, NULL, 1, &(VkImageMemoryBarrier){
-            .sType               = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER,
-            .srcAccessMask       = VK_ACCESS_SHADER_WRITE_BIT,
-            .oldLayout           = VK_IMAGE_LAYOUT_GENERAL,
-            .newLayout           = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR,
-            .srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED,
-            .dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED,
-            .image               = image,
-            .subresourceRange    = range,
-        });
+    imageBarrier(cb,
+        renderImage,
+        VK_IMAGE_LAYOUT_GENERAL, VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL,
+        VK_ACCESS_SHADER_WRITE_BIT, VK_ACCESS_TRANSFER_READ_BIT,
+        VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT, VK_PIPELINE_STAGE_TRANSFER_BIT);
+
+    vkCmdBlitImage(cb,
+        renderImage, VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL,
+        swapImage, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+        1, &blit,
+        VK_FILTER_NEAREST);
+
+    imageBarrier(cb,
+        renderImage,
+        VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL, VK_IMAGE_LAYOUT_GENERAL,
+        VK_ACCESS_TRANSFER_READ_BIT, 0,
+        VK_PIPELINE_STAGE_TRANSFER_BIT, VK_PIPELINE_STAGE_BOTTOM_OF_PIPE_BIT);
+
+    imageBarrier(cb,
+        swapImage,
+        VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, VK_IMAGE_LAYOUT_PRESENT_SRC_KHR,
+        VK_ACCESS_TRANSFER_WRITE_BIT, 0,
+        VK_PIPELINE_STAGE_TRANSFER_BIT, VK_PIPELINE_STAGE_BOTTOM_OF_PIPE_BIT);
 
     vkEndCommandBuffer(cb);
+}
+
+static void createRenderImage(void)
+{
+    VkFormatProperties renderProps;
+    VkFormatProperties swapProps;
+    vkGetPhysicalDeviceFormatProperties(physicalDevice, RENDER_FORMAT, &renderProps);
+    vkGetPhysicalDeviceFormatProperties(physicalDevice, SWAP_FORMAT, &swapProps);
+
+    if ((renderProps.optimalTilingFeatures & VK_FORMAT_FEATURE_STORAGE_IMAGE_BIT) == 0 ||
+        (renderProps.optimalTilingFeatures & VK_FORMAT_FEATURE_BLIT_SRC_BIT) == 0 ||
+        (swapProps.optimalTilingFeatures & VK_FORMAT_FEATURE_BLIT_DST_BIT) == 0)
+        abort();
+
+    vkCreateImage(device, &(VkImageCreateInfo){
+        .sType         = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO,
+        .imageType     = VK_IMAGE_TYPE_2D,
+        .format        = RENDER_FORMAT,
+        .extent        = {
+            .width  = swapExtent.width,
+            .height = swapExtent.height,
+            .depth  = 1,
+        },
+        .mipLevels     = 1,
+        .arrayLayers   = 1,
+        .samples       = VK_SAMPLE_COUNT_1_BIT,
+        .tiling        = VK_IMAGE_TILING_OPTIMAL,
+        .usage         = VK_IMAGE_USAGE_STORAGE_BIT | VK_IMAGE_USAGE_TRANSFER_SRC_BIT,
+        .sharingMode   = VK_SHARING_MODE_EXCLUSIVE,
+        .initialLayout = VK_IMAGE_LAYOUT_UNDEFINED,
+    }, NULL, &renderImage);
+
+    VkMemoryRequirements req;
+    vkGetImageMemoryRequirements(device, renderImage, &req);
+    vkAllocateMemory(device, &(VkMemoryAllocateInfo){
+        .sType           = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO,
+        .allocationSize  = req.size,
+        .memoryTypeIndex = memoryType(req.memoryTypeBits, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT),
+    }, NULL, &renderMemory);
+    vkBindImageMemory(device, renderImage, renderMemory, 0);
+
+    vkCreateImageView(device, &(VkImageViewCreateInfo){
+        .sType            = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO,
+        .image            = renderImage,
+        .viewType         = VK_IMAGE_VIEW_TYPE_2D,
+        .format           = RENDER_FORMAT,
+        .subresourceRange = {
+            .aspectMask = VK_IMAGE_ASPECT_COLOR_BIT,
+            .levelCount = 1,
+            .layerCount = 1,
+        },
+    }, NULL, &renderImageView);
+}
+
+static void updateRenderDescriptor(void)
+{
+    vkUpdateDescriptorSets(device, 1, &(VkWriteDescriptorSet){
+        .sType           = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET,
+        .dstSet          = descriptorSet,
+        .descriptorCount = 1,
+        .descriptorType  = VK_DESCRIPTOR_TYPE_STORAGE_IMAGE,
+        .pImageInfo      = &(VkDescriptorImageInfo){
+            .imageView   = renderImageView,
+            .imageLayout = VK_IMAGE_LAYOUT_GENERAL,
+        },
+    }, 0, NULL);
 }
 
 int main(void)
@@ -133,16 +277,19 @@ int main(void)
 
     VkSurfaceCapabilitiesKHR caps;
     vkGetPhysicalDeviceSurfaceCapabilitiesKHR(physicalDevice, surface, &caps);
+    if ((caps.supportedUsageFlags & VK_IMAGE_USAGE_TRANSFER_DST_BIT) == 0)
+        abort();
+
     swapExtent = caps.currentExtent;
     vkCreateSwapchainKHR(device, &(VkSwapchainCreateInfoKHR){
         .sType            = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR,
         .surface          = surface,
         .minImageCount    = SWAP_IMAGE_COUNT,
-        .imageFormat      = VK_FORMAT_B8G8R8A8_UNORM,
+        .imageFormat      = SWAP_FORMAT,
         .imageColorSpace  = VK_COLOR_SPACE_SRGB_NONLINEAR_KHR,
         .imageExtent      = swapExtent,
         .imageArrayLayers = 1,
-        .imageUsage       = VK_IMAGE_USAGE_STORAGE_BIT,
+        .imageUsage       = VK_IMAGE_USAGE_TRANSFER_DST_BIT,
         .imageSharingMode = VK_SHARING_MODE_EXCLUSIVE,
         .preTransform     = caps.currentTransform,
         .compositeAlpha   = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR,
@@ -151,19 +298,7 @@ int main(void)
     }, NULL, &swapchain);
 
     vkGetSwapchainImagesKHR(device, swapchain, &(uint32_t){SWAP_IMAGE_COUNT}, swapImages);
-
-    for (uint32_t i = 0; i < SWAP_IMAGE_COUNT; i++)
-        vkCreateImageView(device, &(VkImageViewCreateInfo){
-            .sType            = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO,
-            .image            = swapImages[i],
-            .viewType         = VK_IMAGE_VIEW_TYPE_2D,
-            .format           = VK_FORMAT_B8G8R8A8_UNORM,
-            .subresourceRange = {
-                .aspectMask = VK_IMAGE_ASPECT_COLOR_BIT,
-                .levelCount = 1,
-                .layerCount = 1,
-            },
-        }, NULL, &swapImageViews[i]);
+    createRenderImage();
 
     vkCreateDescriptorSetLayout(device, &(VkDescriptorSetLayoutCreateInfo){
         .sType        = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO,
@@ -191,6 +326,7 @@ int main(void)
         .descriptorSetCount = 1,
         .pSetLayouts        = &descriptorSetLayout,
     }, &descriptorSet);
+    updateRenderDescriptor();
 
     vkCreatePipelineLayout(device, &(VkPipelineLayoutCreateInfo){
         .sType                  = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO,
@@ -248,6 +384,7 @@ int main(void)
     }, NULL, &inFlight);
 
     double startTime = nowSeconds();
+    uint32_t frameIndex = 0;
 
     while (rtrPumpEventsOnce() == 0) {
         vkWaitForFences(device, 1, &inFlight, VK_TRUE, UINT64_MAX);
@@ -256,27 +393,20 @@ int main(void)
         uint32_t imageIndex;
         vkAcquireNextImageKHR(device, swapchain, UINT64_MAX, imageAvailable, VK_NULL_HANDLE, &imageIndex);
 
-        vkUpdateDescriptorSets(device, 1, &(VkWriteDescriptorSet){
-            .sType           = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET,
-            .dstSet          = descriptorSet,
-            .descriptorCount = 1,
-            .descriptorType  = VK_DESCRIPTOR_TYPE_STORAGE_IMAGE,
-            .pImageInfo      = &(VkDescriptorImageInfo){
-                .imageView   = swapImageViews[imageIndex],
-                .imageLayout = VK_IMAGE_LAYOUT_GENERAL,
-            },
-        }, 0, NULL);
-
         float time = (float)(nowSeconds() - startTime);
+        VkImageLayout renderOldLayout = frameIndex == 0 ?
+            VK_IMAGE_LAYOUT_UNDEFINED :
+            VK_IMAGE_LAYOUT_GENERAL;
 
         vkResetCommandBuffer(cb, 0);
-        recordCommandBuffer(cb, descriptorSet, swapImages[imageIndex], time);
+        recordCommandBuffer(cb, descriptorSet, renderImage, swapImages[imageIndex], renderOldLayout, time);
+        frameIndex++;
 
         vkQueueSubmit(queue, 1, &(VkSubmitInfo){
             .sType                = VK_STRUCTURE_TYPE_SUBMIT_INFO,
             .waitSemaphoreCount   = 1,
             .pWaitSemaphores      = &imageAvailable,
-            .pWaitDstStageMask    = &(VkPipelineStageFlags){ VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT },
+            .pWaitDstStageMask    = &(VkPipelineStageFlags){ VK_PIPELINE_STAGE_TRANSFER_BIT },
             .commandBufferCount   = 1,
             .pCommandBuffers      = &cb,
             .signalSemaphoreCount = 1,
